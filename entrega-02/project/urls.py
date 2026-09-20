0945d905756445a58f61853424c8d28a65230ebd
jafv-jojo <jafv@cesar.school>
2026-09-20 16:45:48 -0300

    cria infraestrutura para imagens

--- entrega-02/project/urls.py
+++ entrega-02/project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic.base import RedirectView
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +27,8 @@ urlpatterns = [
     path('perguntas/', include('perguntas.urls')),
     path('', RedirectView.as_view(url='/perguntas/', permanent=False), name='home'),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
